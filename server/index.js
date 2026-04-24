257f5e571cbc15ed603a4caa31405c5fdcd6e55d
elian-olbz <mmoriones7@gmail.com>
2026-04-24 11:42:55 +0800

    limit cors origin

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ require('dotenv').config();
 
 const app = express();
 
-// app.use(cors({
-//   origin: [ process.env.FRONTEND_URL,
-//       "https://www.ayosdocs.com",
-//     "https://ayosdocs.com"
-//   ]
-// }));
+app.use(cors({
+  origin: [ process.env.FRONTEND_URL,
+      "https://www.ayosdocs.com",
+    "https://ayosdocs.com"
+  ]
+}));
 
-app.use(cors({ origin: '*' }));
+//app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: true }));
