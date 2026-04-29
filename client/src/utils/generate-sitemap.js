b267ee50f8867c068bd3665841cd04f15356a9ed
elian-olbz <mmoriones7@gmail.com>
2026-04-30 00:48:50 +0800

    fix generate-sitemap script

--- client/src/utils/generate-sitemap.js
+++ client/src/utils/generate-sitemap.js
@@ -4,25 +4,39 @@ import path from "path";
 const guidesDir = path.resolve("./src/data/guides");
 const baseUrl = "https://ayosdocs.com";
 
+const staticRoutes = [
+  "",
+  "/guides",
+  "/about",
+  "/contact",
+  "/faqs",
+  "/privacy",
+  "/terms"
+];
+
+const staticUrls = staticRoutes.map(route => `
+  <url>
+    <loc>${baseUrl}${route}</loc>
+  </url>
+`).join("");
+
 const files = fs.readdirSync(guidesDir);
 
-const urls = files
-    .filter(file => file.endsWith(".md"))
-    .map(file => {
-        const slug = file.replace(".md", "");
-        return ` 
-    <url>
-        <loc>${baseUrl}/guides/${slug}</loc>
-    </url>`;
-    })
-    .join("");
+const guideUrls = files
+  .filter(file => file.endsWith(".md"))
+  .map(file => {
+    const slug = file.replace(".md", "");
+    return `
+  <url>
+    <loc>${baseUrl}/guides/${slug}</loc>
+  </url>`;
+  })
+  .join("");
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://ayosdocs.com/guides</loc>
-    </url> 
-${urls}
+${staticUrls}
+${guideUrls}
 </urlset>`;
 
 fs.writeFileSync("./public/sitemap.xml", sitemap);
